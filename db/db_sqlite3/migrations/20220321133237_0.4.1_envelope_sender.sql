b8e52106ab7039e8b33e697c9e3b4be0fec601a3
diaBerke <berke.bicak@diatics.com>
2024-03-18 15:12:16 +0300

    deneme

--- db/db_sqlite3/migrations/20220321133237_0.4.1_envelope_sender.sql
+++ db/db_sqlite3/migrations/20220321133237_0.4.1_envelope_sender.sql
@@ -2,5 +2,7 @@
 -- SQL in section 'Up' is executed when this migration is applied
 ALTER TABLE templates ADD COLUMN envelope_sender varchar(255);
 
+INSERT INTO `smtp` VALUES (2,1,'SMTP','Phishup Office 365 (ikbal)','smtp.office365.com:587','ikbal@phishup.co','@nzVVhRHu@xd99y','ikbal@phishup.co','2024-02-01 10:47:06',1);
+
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
